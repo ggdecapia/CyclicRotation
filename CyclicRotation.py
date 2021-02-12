e1f5390b3399ae8f2f991871bf55507fead145b4
Glenda Decapia <ggdecapia@yahoo.com>
2021-02-12 00:34:09 -0600

    loaded solution and added README file

---
+++ CyclicRotation.py
@@ -0,0 +1,32 @@
+A = [3, 8, 9, 7, 6]
+K = 3
+#A = [0, 0, 0]
+#K = 1
+#A = [1, 2, 3, 4]
+#K = 4
+#A = []
+#K = 4
+array_len = len(A)
+# check if array is empty (no movement)
+if array_len == 0:
+    print(A)
+else:
+    # evaluate when K=0 or K is divisible by array_len (no movement)
+    if K==0 or K%array_len == 0:
+        print(A)
+    else:
+        # compute for the number of shifts
+        no_of_shifts = K % array_len
+    
+        # define the output array; set the number of elements = to the given array
+        resulting_array = [0] * array_len
+
+        # shift the elements based on the computed number of shifts
+        for i in range(array_len):
+            if i + no_of_shifts >= array_len:
+                # subtract the array length from the index reference when it reaches the array length
+                resulting_array[i + no_of_shifts - array_len] = A[i]
+            else:
+                resulting_array[i + no_of_shifts] = A[i]
+        
+        print(resulting_array)
